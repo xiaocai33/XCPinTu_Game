cd99a71399042171ee3149f33d7fe2124700772a
xiaocai <tycai1@sina.com>
2016-05-30 19:43:49 +0800

    实现本地图片的切换

--- PinTu_Game/PinTu_Game/Classes/Main/Controller/MainGameViewController.m
+++ PinTu_Game/PinTu_Game/Classes/Main/Controller/MainGameViewController.m
@@ -7,31 +7,99 @@
 // 主视图控制界面
 
 #import "MainGameViewController.h"
+#import "UIView+SDAutoLayout.h"
 
 @interface MainGameViewController ()
+/** 显示要使用的图片 */
+@property (nonatomic, weak) UIImageView *imageView;
+
+@property (nonatomic, assign) int currentImageID;
 
 @end
 
 @implementation MainGameViewController
+#pragma mark - 懒加载控件
+- (UIImageView *)imageView{
+    if (!_imageView) {
+        UIImageView *imageView = [[UIImageView alloc] init];
+        [self.view addSubview:imageView];
+        _imageView = imageView;
+    }
+    return _imageView;
+}
 
+#pragma mark - 控制器相关
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor greenColor];
+    self.currentImageID = 0;
+    [self setupChildView];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - 初始化控件
+- (void)setupChildView{
+    //设置背景图片
+    UIImageView *backgroundImageView = [[UIImageView alloc] init];
+    backgroundImageView.image = [UIImage imageNamed:@"bg"];
+    [self.view addSubview:backgroundImageView];
+    backgroundImageView.sd_layout.leftEqualToView(self.view).rightEqualToView(self.view).topEqualToView(self.view).bottomEqualToView(self.view);
+    
+    //设置显示的拼图
+    UIImageView *contextImageView = [[UIImageView alloc] init];
+    contextImageView.image = [UIImage imageNamed:@"pin_0"];
+    [self.view addSubview:contextImageView];
+    _imageView = contextImageView;
+    contextImageView.sd_layout.rightSpaceToView(self.view, 10).leftSpaceToView(self.view, 10).topSpaceToView(self.view, 20).bottomSpaceToView(self.view, 60);
+    
+    //添加按钮(开始,选择图片)
+    UIButton *startBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    [startBtn setTitle:@"开始" forState:UIControlStateNormal];
+    [startBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [self.view addSubview:startBtn];
+    startBtn.sd_layout.leftSpaceToView(self.view, 100).topSpaceToView(contextImageView, 10).widthIs(80).heightIs(44);
+    
+    UIButton *chooseImageBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    [chooseImageBtn setTitle:@"选择图片" forState:UIControlStateNormal];
+    [chooseImageBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [chooseImageBtn addTarget:self action:@selector(chooseImageBtnClick) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:chooseImageBtn];
+    chooseImageBtn.sd_layout.leftSpaceToView(startBtn, 20).topSpaceToView(contextImageView, 10).widthIs(80).heightIs(44);
+    
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+#pragma mark - 选择图片
+- (void)chooseImageBtnClick{
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"请选择图片来源" message:@"" preferredStyle:UIAlertControllerStyleActionSheet];
+    
+    UIAlertAction *nextImage = [UIAlertAction actionWithTitle:@"下一张" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        self.imageView.image = [UIImage imageNamed:[self nextImage]];
+    }];
+    
+    [alert addAction:nextImage];
+    [self presentViewController:alert animated:YES completion:nil];
 }
-*/
+
+/** 从本地取出下一张图片 */
+- (NSString *)nextImage{
+    if (self.currentImageID < 8) {
+        self.currentImageID++;
+    }else{
+        self.currentImageID = 0;
+    }
+    return [NSString stringWithFormat:@"pin_%zd", self.currentImageID];
+}
+
+
+#pragma mark - 设置状态栏颜色
+- (UIStatusBarStyle)preferredStatusBarStyle{
+    return UIStatusBarStyleLightContent;
+}
+
+#pragma mark - 设置屏幕支持方向(竖屏)
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations{
+    return UIInterfaceOrientationMaskPortrait;
+}
+
+
+
 
 @end
